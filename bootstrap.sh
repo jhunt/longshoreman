71d4810e9aeeb2eaf410196db4d0ab4198dc18e5
James Hunt <james@niftylogic.com>
2016-02-01 21:23:43 -0500

    Update *rc environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,7 +38,94 @@ go get github.com/onsi/ginkgo
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 
-#go get github.com/starkandwayne/training/go/longshoreman/backend
+cat >/home/vagrant/.vimrc <<EOS
+set bg=dark
+set visualbell t_vb=
+set novisualbell
+filetype indent on
+set autoindent
+set backspace=start,eol,indent
+set formatoptions=tcqo
+set hlsearch
+hi LineNr ctermfg=DarkGrey
+set number
+set ts=4 sts=4 sw=4
+set noet
+set laststatus=2
+set stl=[%n]\ file:%-50F\ %y%r%m\ byte:0x%B%=line:%l:%c/%L\ %p%%
+EOS
+
+cat >/home/vagrant/.inputrc <<EOS
+set bell-style none
+set completion-ignore-case off
+set mark-directories on
+set mark-symlinked-directories on
+set match-hidden-files off
+set menu-complete-display-prefix on
+set print-completions-horizontally on
+set colored-stats on
+EOS
+
+cat >/home/vagrant/.tmux.conf <<EOS
+set -g utf8
+set-window-option -g utf8 on
+set-window-option -g mode-key vi
+set -g history-limit 50000
+unbind C-b
+set -g prefix 'C-a'
+bind 'C-a' send-prefix
+bind - split-window -v
+bind + split-window -h
+bind h select-pane -L
+bind j select-pane -D
+bind k select-pane -U
+bind l select-pane -R
+bind -r C-h select-window -t :-
+bind -r C-l select-window -t :+
+# screen ^C c
+unbind ^C
+unbind  c
+bind ^C new-window
+bind  c new-window
+unbind ^D
+bind ^D detach
+unbind ^N
+unbind  n
+bind -r ^N next-window
+unbind ^P
+unbind  p
+bind -r ^P previous-window
+unbind K
+unbind k
+bind K confirm-before "kill-window"
+bind k confirm-before "kill-window"
+unbind ^L
+unbind  l
+bind ^L refresh-client
+bind  l refresh-client
+unbind |
+bind | split-window
+unbind '"'
+bind '"' choose-window
+set -g status-utf8 on
+set -g status-bg black
+set -g status-fg white
+set -g status-left-length 70
+set -g status-left "#[fg=colour192]#h:"
+set -g status-right-length 60
+set -g status-right "#[fg=yellow]%Z %z"
+set-option -g pane-border-fg colour235
+set-option -g pane-active-border-fg colour88
+set-option -g default-terminal "screen"
+set -g status-justify centre
+set-window-option -g window-status-fg brightblue
+set-window-option -g window-status-bg black
+set-window-option -g window-status-attr dim
+set-window-option -g window-status-current-fg brightgreen
+set-window-option -g window-status-current-bg black
+set-window-option -g window-status-current-attr bright
+EOS
+
 EOF
 sudo -u vagrant bash /tmp/user-setup.sh
 rm -f /tmp/user-setup.sh
